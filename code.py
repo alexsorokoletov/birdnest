b4c46a62a272e57aaa73d52e85baf40333fc15f0
wiennat <wiennat@a24b8963-e14c-0410-879a-37a89a7505c4>
2008-05-10 15:56:28 +0000

    Initial commit. Birdnest is currently working as a twitter proxy without any filter rule.

---
+++ code.py
@@ -0,0 +1,74 @@
+import wsgiref.handlers
+import logging
+from google.appengine.api import urlfetch
+from google.appengine.ext import webapp
+
+twitterAPI = "http://twitter.com/"
+
+class Filter():
+  def filter(self, returnText, method):
+    pass
+
+class IncludeImageFilter(Filter):
+  def filter(self, returnText, method):
+    pass
+
+class TextOnlyFilter(IncludeImageFilter):
+  def filter(self, returnText, method):
+    pass
+
+class BaseProxy(webapp.RequestHandler):
+  def __init__(self):
+    self.filter = Filter()
+    self.required_header = ['Authorization', 'User-Agent', 'X-Twitter-Client', 'X-Twitter-Client-URL', 'X-Twitter-Client-Version']
+
+  def get(self, params):    
+    url = twitterAPI + params
+    headers = {}
+    for header in self.required_header:
+      if self.request.headers.has_key(header):
+        headers[header] = self.request.headers[header]
+
+    result = urlfetch.fetch(url, headers=headers)
+    self.sendoutput(result)
+
+  def sendoutput(self, result):
+    if result.status_code == 200:
+      self.response.headers = result.headers
+      self.response.out.write(result.content)
+    else:
+      self.error(result.status_code)
+      self.response.out.write('')
+    
+    
+  def post(self, params):
+    url = twitterAPI + params
+    headers = {}
+    for header in self.required_header:
+      if self.request.headers.has_key(header):
+        headers[header] = self.request.headers[header]
+    try:
+      result = urlfetch.fetch(url, payload=self.request.body, method=urlfetch.POST, headers=headers)
+      self.sendoutput(result)
+    except Exception, inst:
+        self.error(500)
+        logging.error("%s \n\n %s \n\n %s " % ( inst, self.request.headers, self.request.body))
+
+class TextOnlyProxy(BaseProxy):
+    def __init__(self):
+      self.filter = TextOnlyFilter()
+
+class IncludeImageProxy(BaseProxy):
+    def __init__(self):
+      self.filter = IncludeImageFilter()
+
+def main():
+  application = webapp.WSGIApplication(
+                                       [('/api/(.*)', BaseProxy),
+                                       ('/text/(.*)', TextOnlyProxy),
+                                       ('/image/(.*)', IncludeImageProxy)],
+                                       debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == "__main__":
+  main()

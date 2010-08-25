26e7c15b0a0f87aa2cad59e7812f72deee599e15
wiennat <wiennat@a24b8963-e14c-0410-879a-37a89a7505c4>
2010-08-25 23:36:54 +0000

    Change api endpoint to the new https://api.twitter.com/1. Note that, using OAuth might breaks TwitPic and geolocation proxying.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,17 +1,7 @@
 import os, sys
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), "nest"))
 sys.path.insert(0, path)
-#import web
-#import nest.code
-#application = web.application(nest.code.urls, globals()).wsgifunc()
 import socket
-#ua = os.environ.get("HTTP_USER_AGENT", "")
-#if ua.find('jibjib') >= 0:
-#  socket.setdefaulttimeout(15)
-#elif ua.find('zh-CN') >= 0:
-#  sys.exit(0)
-#else:
-#  socket.setdefaulttimeout(2)
 socket.setdefaulttimeout(60)
 import code
 code.init_logger(os.path.dirname(__file__))
